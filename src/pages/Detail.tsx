eef3b755507d6e2c8961342df8fa2058ba11d1e5
ddooo00 <1995thdud@naver.com>
2023-08-08 12:42:50 +0900

    중간저장

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
-const Detail = () => {
+function Detail() {
   return <div>Detail</div>;
-};
+}
 
 export default Detail;
--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import supabase from '../lib/supabase';
+
+interface Comment {
+  id: number;
+  nickname: string;
+  date: number;
+}
 
 const Mypage = () => {
-  return <div>Mypage</div>;
+  const [comments, setComments] = useState<Comment[]>([]);
+  const [newComment, setNewComment] = useState<string>('');
+
+  useEffect(() => {
+    fetchComments();
+  }, []); // Fetch comments on initial render
+
+  const handleCommentSubmit = async () => {
+    try {
+      const { data, error } = await supabase
+        .from('comment')
+        .insert([{ nickname: 'User', date: Date.now(), text: newComment }]);
+
+      if (error) {
+        console.error('Error adding comment:', error);
+      } else {
+        console.log('Comment added successfully:', data);
+        setNewComment(''); // Clear the input field
+        fetchComments(); // Fetch comments again to include the new comment
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchComments = async () => {
+    try {
+      const { data } = await supabase.from('comment').select('*');
+      console.log('data', data);
+      setComments(data as Comment[]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Detail</h1>
+      <div>
+        <input type="text" value={newComment} onChange={(e) => setNewComment(e.target.value)} placeholder="댓글 작성" />
+        <button onClick={handleCommentSubmit}>작성하기</button>
+      </div>
+      {comments.map((comment) => (
+        <div key={comment.id}>
+          {comment.nickname}, {new Date(comment.date).toLocaleString()}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Mypage;
