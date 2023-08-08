296046c0f2e55ed8a5510a5194ebe14a8cc7b4a8
Jiwon Park <xoxojw.dev@gmail.com>
2023-08-08 20:20:17 +0900

    design: 챗봇 UI 설정 및 prompt를 input에서 textarea로 변경

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -8,7 +8,18 @@ import Studyboard from '../pages/Studyboard';
 import Freeboard from '../pages/Freeboard';
 import Write from '../pages/Write';
 
+import { useSelector, useDispatch } from 'react-redux';
+import { ChatBotState, toggleChatBotState } from '../redux/module/chatBotUISlice';
+import ChatBot from '../components/chatbot/ChatBot';
+import ChatIcon from "remixicon-react/QuestionAnswerFillIcon"
+import { styled } from 'styled-components';
+
 const Router = () => {
+  const chatBotIsActive = useSelector((state: { chatBotUI: ChatBotState; }) => state.chatBotUI.chatBotIsActive);
+  const dispatch = useDispatch();
+  const toggleChatBot = () => {
+    dispatch(toggleChatBotState());
+  }
   return (
     <BrowserRouter>
       <Header />
@@ -21,7 +32,30 @@ const Router = () => {
         <Route path="/write" element={<Write />} />
       </Routes>
       <Footer />
+      <ChatBotButton onClick={toggleChatBot}>
+        <ChatIcon color='#fff' size={20} />
+      </ChatBotButton>
+      {chatBotIsActive ? <ChatBot /> : null}
     </BrowserRouter>
   );
 };
 export default Router;
+
+const ChatBotButton = styled.button`
+  width: 50px;
+  height: 50px;
+  border: none;
+  border-radius: 50%;
+  background-color: #1f3646;
+  box-shadow: 5px 5px 13px rgba(154, 154, 154, 0.4);
+  cursor: pointer;
+  position: fixed;
+  bottom: 20px;
+  z-index: 9999;
+  right: 30px;
+  transition: all 0.1s ease-in-out;
+
+  &:hover {
+    background-color: #445e70;
+  }
+`
