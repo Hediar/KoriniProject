88c88a1fccca4888d113b77abe0c0e8cf78c5295
Jiwon Park <xoxojw.dev@gmail.com>
2023-08-10 18:18:48 +0900

    feat: Private routes 설정 (로그인 여부에 따라)

--- src/shared/PrivateRoute.tsx
+++ src/shared/PrivateRoute.tsx
@@ -1,11 +1,10 @@
+import { Outlet, Navigate } from 'react-router-dom';
 import { useAppSelector } from '../hooks';
 import { RootState } from '../redux/config/configStore';
-import { Outlet, Navigate } from 'react-router-dom';
 
 const PrivateRoute = () => {
   const { user } = useAppSelector((state: RootState) => state.user);
-
   return user ? <Outlet /> : <Navigate to="/" />
-}
+};
 
 export default PrivateRoute;
