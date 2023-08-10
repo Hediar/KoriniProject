e17eac6d0fbc89dee2db31484a16d8dbeca28bdb
Seryeong Lee <srlimvp@gmail.com>
2023-08-10 09:48:26 +0900

    docs: Mainposts.tsx 코드 정리

--- src/components/main/Mainposts.tsx
+++ src/components/main/Mainposts.tsx
@@ -1,14 +1,10 @@
-import { PostType, ToTalDataType } from '../../types/types';
-import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
-import React, { useEffect, useState } from 'react';
-import { getDataNumber, getPosts } from '../../api/post';
+import { ToTalDataType } from '../../types/types';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import React, { useMemo } from 'react';
+import { getPosts } from '../../api/post';
 import { useInView } from 'react-intersection-observer';
 
 const Mainposts = () => {
-  // 페이지 정보 만들기
-  // const { data: totalCount } = useQuery<any>(['mainCount'], getDataNumber);
-  // const [dataState, setDataState] = useState(1);
-
   const {
     data: posts,
     isLoading,
@@ -20,9 +16,6 @@ const Mainposts = () => {
     queryKey: ['posts'],
     queryFn: ({ pageParam }) => getPosts(pageParam),
     getNextPageParam: (lastPage) => {
-      console.log('lastPage', lastPage);
-      // console.log('allPages', allPages);
-
       // 전체 페이지 개수보다 작을 때 다음 페이지로
       if (lastPage.page < lastPage.total_pages) {
         return lastPage.page + 1;
@@ -30,7 +23,6 @@ const Mainposts = () => {
     }
   });
 
-  console.log('hasNextPage:', hasNextPage);
   const { ref } = useInView({
     threshold: 1, // 맨 아래에 교차가 됐을 때
     onChange: (inView) => {
@@ -41,9 +33,18 @@ const Mainposts = () => {
     }
   });
 
-  useEffect(() => {
-    console.log('posts', posts);
+  // select 역할, posts데이터 가공
+  const processedPosts = useMemo(() => {
+    return posts?.pages
+      .map((data) => {
+        return data.posts;
+      })
+      .flat();
   }, [posts]);
+
+  // useEffect(() => {
+  //   console.log('posts', posts);
+  // }, [posts]);
 
   if (isLoading) {
     return <h1>로딩중입니다</h1>;
@@ -53,41 +54,34 @@ const Mainposts = () => {
   }
   return (
     <>
-      {posts?.pages
-        .map((data) => {
-          return data.posts;
-        })
-        .flat()
-        .map((post) => {
-          return (
-            <>
-              -----------------------
-              <div key={post.postid}>
-                {post.postid}
-                <div>user id: {post.userid}</div>
-                <div>태그: {post.tag}</div>
-                <div>제목: {post.title}</div>
-                <div>작성자: {post.name}</div>
-                <div>내용: {post.body}</div>
-                <div>카테고리: {post.category}</div>
-                <div>작성날짜: {post.date}</div>
-              </div>
-              -----------------------
-            </>
-          );
-        })}
+      {processedPosts?.map((post) => {
+        return (
+          <>
+            -----------------------
+            <div key={post.postid}>
+              {post.postid}
+              <div>user id: {post.userid}</div>
+              <div>태그: {post.tag}</div>
+              <div>제목: {post.title}</div>
+              <div>작성자: {post.name}</div>
+              <div>내용: {post.body}</div>
+              <div>카테고리: {post.category}</div>
+              <div>작성날짜: {post.date}</div>
+            </div>
+            -----------------------
+          </>
+        );
+      })}
       <div
         style={{
           textAlign: 'center',
-          backgroundColor: 'green',
+          backgroundColor: 'white',
           color: 'white',
           width: '100%',
           height: 50
         }}
         ref={ref}
-      >
-        Trigger to Fetch Here
-      </div>
+      ></div>
     </>
   );
 };
