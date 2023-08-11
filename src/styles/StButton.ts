3770c4af7859ce8fac297536b2f23d7b997a5eca
Jiwon Park <xoxojw.dev@gmail.com>
2023-08-11 19:59:16 +0900

    feat: Top scroll button 추가

--- src/styles/StButton.ts
+++ src/styles/StButton.ts
@@ -14,3 +14,22 @@ export const Button = styled.button<{ width?: string; height?: string }>`
     background-color: ${(props) => props.theme.buttonHoverColor};
   }
 `;
+
+export const TopButton = styled.button`
+  width: 50px;
+  height: 50px;
+  border: none;
+  border-radius: 50%;
+  background-color: #1f3646;
+  box-shadow: 5px 5px 13px rgba(154, 154, 154, 0.4);
+  cursor: pointer;
+  position: fixed;
+  bottom: 20px;
+  z-index: 9999;
+  right: 30px;
+  transition: all 0.1s ease-in-out;
+
+  &:hover {
+    background-color: ${props => props.theme.mainNavyColor};
+  }
+`;
