8f8a0a6307bd198110097e85dedb2ae7d356da48
Jiwon Park <xoxojw.dev@gmail.com>
2023-08-11 12:42:58 +0900

    refactor: 라우터 컴포넌트에 있던 챗봇 버튼 스타일 컴포넌트 분리

--- src/styles/StChatBot.ts
+++ src/styles/StChatBot.ts
@@ -1,5 +1,24 @@
 import styled from "styled-components";
 
+export const ChatBotButton = styled.button`
+  width: 50px;
+  height: 50px;
+  border: none;
+  border-radius: 50%;
+  background-color: #1f3646;
+  box-shadow: 5px 5px 13px rgba(154, 154, 154, 0.4);
+  cursor: pointer;
+  position: fixed;
+  bottom: 20px;
+  z-index: 9999;
+  right: 30px;
+  transition: all 0.1s ease-in-out;
+
+  &:hover {
+    background-color: #445e70;
+  }
+`;
+
 export const ChatContainer = styled.div`
   display: flex;
   flex-direction: column;
